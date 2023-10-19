13b967bbd3b32dfc1f347ae12fd8a9a4ba48c8c5
Egor Kovalevich <poshtakem04@gmail.com>
2023-10-20 00:31:13 +0300

    main_part_three

---
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.utils
+
+class Constants {
+    companion object{
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "410d06db4db64f28a6635b213cb7fa1a"
+    }
+}
