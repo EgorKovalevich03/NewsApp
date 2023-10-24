961e080dfb20924fd99366bdb5d092b2952387b7
Egor Kovalevich <poshtakem04@gmail.com>
2023-10-24 23:17:23 +0300

    main_part_eight

--- app/src/main/java/com/example/newsapp/data/db/ArticleDao.kt
+++ app/src/main/java/com/example/newsapp/data/db/ArticleDao.kt
@@ -1,21 +1,22 @@
 package com.example.newsapp.data.db
 
-import androidx.lifecycle.LiveData
+
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import com.example.newsapp.models.Article
 import androidx.room.Query
 
+@Dao
 interface ArticleDao {
 
     @Query("SELECT * FROM articles")
-    suspend fun getAllArticles(): LiveData<List<Article>>
+    fun getAllArticles(): List<Article>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(article: Article)
 
     @Delete
     suspend fun delete(article: Article)
-
 }
--- app/src/main/java/com/example/newsapp/di/AppModule.kt
+++ app/src/main/java/com/example/newsapp/di/AppModule.kt
@@ -1,10 +1,15 @@
 package com.example.newsapp.di
 
+import android.content.Context
+import androidx.room.Room
 import com.example.newsapp.data.api.NewsService
+import com.example.newsapp.data.db.ArticleDao
+import com.example.newsapp.data.db.ArticleDatabase
 import com.example.newsapp.utils.Constants.Companion.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -15,15 +20,18 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
     @Provides
     fun baseUrl() = BASE_URL
 
+    @Provides
+    fun logging() = HttpLoggingInterceptor()
+        .setLevel(HttpLoggingInterceptor.Level.BODY)
 
     @Provides
-    fun logging() = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-
-    @Provides
-    fun okHttpClient() = OkHttpClient.Builder().addInterceptor(logging()).build()
+    fun okHttpClient() = OkHttpClient.Builder()
+        .addInterceptor(logging())
+        .build()
 
     @Provides
     @Singleton
@@ -35,4 +43,17 @@ object AppModule {
             .build()
             .create(NewsService::class.java)
 
+    @Provides
+    @Singleton
+    fun provideAtricleDatabase(@ApplicationContext context: Context) =
+        Room.databaseBuilder(
+            context,
+            ArticleDatabase::class.java,
+            "article_database"
+        ).build()
+
+    @Provides
+    fun provideArticleDao(appDatabase: ArticleDatabase): ArticleDao {
+        return appDatabase.getArticleDao()
+    }
 }
